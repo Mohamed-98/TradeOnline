c7c0847ce4f27876c879b71107dcb63c3cb70777
Mohamed-98 <m.gado86@gmail.com>
2020-07-21 02:35:04 +0200

    New Edit

---
+++ Footer-login.php
@@ -0,0 +1,21 @@
+    <footer
+      style="text-align: center; font-family:Arial, Helvetica, sans-serif; margin-top: 10px;"
+    >
+      Copyright &copy; 2019 by:Hassan Mostafa
+    </footer>
+
+    <script>
+      function showForm() {
+        document.getElementById("show").style.display = "block";
+        document.getElementById("hide").style.display = "none";
+      }
+      function hideForm() {
+        document.getElementById("show").style.display = "none";
+        document.getElementById("hide").style.display = "block";
+      }
+      function backToHome() {
+        window.location.href = "index.html";
+      }
+    </script>
+  </body>
+</html>
